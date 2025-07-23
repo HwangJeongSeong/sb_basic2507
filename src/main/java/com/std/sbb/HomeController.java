413ece966489299d4baf994bfe589627689c4a1f
jeongseong <hjs54336297@gmail.com>
2025-07-23 15:38:22 +0900

    파라미터받기

--- src/main/java/com/std/sbb/HomeController.java
+++ src/main/java/com/std/sbb/HomeController.java
@@ -2,6 +2,7 @@ package com.std.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //@COntrooler의 의미
@@ -21,10 +22,16 @@ public class HomeController {
     //만약에 home/main 이런 요청이 오면 아래 메서드를 실행해줘
     @GetMapping("/home/increase")
     @ResponseBody
-    public String ShowMain() {
+    public String ShowIncrese() {
         a++;
         return ("응답 : " + a);
 
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int plus(@RequestParam(value = "a", defaultValue = "0") int a, @RequestParam(value = "b", defaultValue = "0") int b){
+        return a+b;
     }
 
 }
