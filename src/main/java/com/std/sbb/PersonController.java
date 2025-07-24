35ea92aa804986cd0f063d382e841d47ab90df4f
jeongseong <hjs54336297@gmail.com>
2025-07-24 16:04:56 +0900

    사람 crud

--- src/main/java/com/std/sbb/PersonController.java
+++ src/main/java/com/std/sbb/PersonController.java
@@ -3,34 +3,71 @@ package com.std.sbb;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
+@Controller
 public class PersonController {
-    private int lastId;
-    private List<Person> people;
+    int lastId;
+    List<Person> people;
 
-    public PersonController() {
+    PersonController () {
         lastId = 0;
         people = new ArrayList<>();
     }
 
     @GetMapping("/person/add")
+    @ResponseBody
     public String addPerson(@RequestParam("name") String name, @RequestParam("age") int age) {
         lastId++;
-        Person person = new Person(lastId, age, name);
-        people.add(person);
+        Person p = new Person(lastId, age, name);
 
-        return String.format("id.%d \n이름 : %s \n나이 : %d \n", person.getId(), person.getName(), person.getAge());
+        people.add(p);
+
+        return String.format("%d 번째 사람이 추가 되었습니다.", p.getId());
+    }
+
+    @GetMapping("/person/people")
+    @ResponseBody
+    public List<Person> getPeople() {
+        System.out.println(people);
+        return people;
+    }
+    @GetMapping("/person/remove")
+    @ResponseBody
+    public String removePerson(int id) {
+        boolean removed = people.removeIf(person -> person.getId() == id);
+
+        if (removed == false) {
+            return id + "번 사람이 존재하지 않습니다.";
+        }
+
+        return id + "번 사람이 삭제 되었습니다.";
+    }
+
+    @GetMapping("/person/modify")
+    @ResponseBody
+    public String modifyPerson(@RequestParam("id") int id, @RequestParam("name") String name, @RequestParam("age") int age) {
+        if (id <= 0 || id > people.size()) {
+            return  id + "번 사람이 존재하지 않습니다.";
+        }
+
+        Person p = people.get(id - 1);
+        p.setName(name);
+        p.setAge(age);
+
+        return String.format("%d번 사람의 정보가 수정되었습니다:", p.getId());
     }
 }
 
 @AllArgsConstructor
 @Getter
 @Setter
+@ToString
 class Person {
     private int id;
     private int age;
